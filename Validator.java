afd81836c673010bed2771fbb75895c7290be2b6
Chad Aldrich <chadaldrich93@gmail.com>
2018-06-28 18:16:07 -0700

    Removed tabs from Validator.java

--- Validator.java
+++ Validator.java
@@ -14,12 +14,12 @@ import java.util.Stack;
 //Validator requires all tags must start and end on the same line
 public class Validator{
     
-	static final int BACKSPACE = 10;
-	static final int TAB = 11;
-	static final int LINEFEED = 12;
-	static final int VERTICAL_TAB = 13;
-	static final int NEW_PAGE = 14;
-	static final int CARRIAGE_RETURN = 15;
+    static final int BACKSPACE = 10;
+    static final int TAB = 11;
+    static final int LINEFEED = 12;
+    static final int VERTICAL_TAB = 13;
+    static final int NEW_PAGE = 14;
+    static final int CARRIAGE_RETURN = 15;
     static final int WHITESPACE = 32;
     static final int REC_LINE_LENGTH = 200;
     
@@ -48,10 +48,10 @@ public class Validator{
                 if (v.nestedTags.empty())
                 	break;
             }
-	        if (v.pageHasValue()) {
+            if (v.pageHasValue()) {
                 v.readValue(false,'"');
                 v.readWhiteSpace();
-	        }
+            }
         }
         System.out.println("Xml document is valid");
         v.targetFile.close();
@@ -63,36 +63,36 @@ public class Validator{
     }
     
     int getCurrentLine() {
-    	return currentLine;
+        return currentLine;
     }
     
     char getCurrentChar() {
-    	return currentChar;
+        return currentChar;
     }
     
     void setTargetFile(PushbackReader file) {
-    	targetFile = file;
+        targetFile = file;
     }
     
     boolean isNameFirstChar(char targetChar) {
         if ( ( ('A' <= targetChar) && (targetChar <= 'Z') ) ||
-    	     ( ('a' <= targetChar) && (targetChar <= 'z') ) ||
-    	     (  targetChar == '_')
-    	   )
-    	     return true;
+             ( ('a' <= targetChar) && (targetChar <= 'z') ) ||
+             (  targetChar == '_')
+           )
+             return true;
         return false;
     }
     
     boolean isNameChar(char targetChar, boolean isFirstChar){
         if (isNameFirstChar(targetChar))
-        	return true;
+            return true;
         if (!isFirstChar){
-	        if ('0' <= targetChar && targetChar <= '9' || 
-		        targetChar == ':'                      || 
+            if ('0' <= targetChar && targetChar <= '9' || 
+                targetChar == ':'                      || 
                 targetChar == '.' 
                )
-		    return true;
-	    }
+            return true;
+        }
         return false;
     }
     
@@ -104,12 +104,12 @@ public class Validator{
                               char fourthChar, char fifthChar, char sixthChar){
         //return value is size of char entity
         //0 means none was found
-	    if (firstChar == '&' && secondChar == 'g' && thirdChar == 't' &&
-	    	fourthChar == ';')
+        if (firstChar == '&' && secondChar == 'g' && thirdChar == 't' &&
+            fourthChar == ';')
             return 4;
         if (firstChar == '&' && secondChar == 'l' && thirdChar == 't' &&
             fourthChar == ';')
-        	return 4;
+            return 4;
         if (firstChar == '&' && secondChar == 'a' && thirdChar == 'm' &&
             fourthChar == 'p' && fifthChar == ';')
             return 5;
@@ -130,52 +130,52 @@ public class Validator{
     }
     
     char checkNextChar() throws IOException{
-    	char nextChar = (char)targetFile.read();
-    	targetFile.unread(nextChar);
-    	return nextChar;
+        char nextChar = (char)targetFile.read();
+        targetFile.unread(nextChar);
+        return nextChar;
     }
     
     String readWhiteSpace() throws IOException{
-    	String whiteSpace = "";
+        String whiteSpace = "";
         while ( (checkNextChar() == WHITESPACE)      || 
-        		(checkNextChar() == BACKSPACE)       ||
-        		(checkNextChar() == TAB)             ||
-        		(checkNextChar() == LINEFEED)        ||
-        		(checkNextChar() == VERTICAL_TAB)    ||
-        		(checkNextChar() == NEW_PAGE)        ||
-        		(checkNextChar() == CARRIAGE_RETURN) ||
-        		(checkNextChar() == '\n')
-        	  ) {
-        	whiteSpace += readNextChar();
-        	if ( currentChar == '\n'){
-        		currentColumn = 0;
-        		currentLine += 1;
-        	}
+                (checkNextChar() == BACKSPACE)       ||
+                (checkNextChar() == TAB)             ||
+                (checkNextChar() == LINEFEED)        ||
+                (checkNextChar() == VERTICAL_TAB)    ||
+                (checkNextChar() == NEW_PAGE)        ||
+                (checkNextChar() == CARRIAGE_RETURN) ||
+                (checkNextChar() == '\n')
+              ) {
+            whiteSpace += readNextChar();
+            if ( currentChar == '\n'){
+                currentColumn = 0;
+                currentLine += 1;
+            }
         }
         return whiteSpace;
     }
 
     //all read functions return an empty string to indicate entity was not read
     String readName(String nameType) throws IOException{
-    	String name = "";
+        String name = "";
         boolean isFirstChar = true;
         while (true){
             char nextChar = checkNextChar();
             boolean nextIsName = isNameChar(nextChar, isFirstChar);
             if (nextIsName){
-            	name += readNextChar();
+                name += readNextChar();
                 isFirstChar = false;
                 continue;
-	        }
+            }
             else if ( ( (nextChar == '>') || (nextChar == ' ') 
-            		  )  && 
-            		    (nameType == "tag") 
-            		)
+                      )  && 
+                        (nameType == "tag") 
+                    )
                 return name;
             else if ( (nextChar == '=') && (nameType == "attribute") )
-            	return name;
-	        else
-	            return "";
+                return name;
+            else
+                return "";
         }
     }
     
@@ -188,51 +188,51 @@ public class Validator{
     }
     
     int checkEntityReference() throws IOException{
-    	int start = currentColumn;
-    	char first = readNextChar();
-    	char second = readNextChar();
-    	char third = readNextChar();
-    	char fourth = readNextChar();
-    	char fifth = readNextChar();
-    	char sixth = readNextChar();
-    	char[] returnBuffer = {first, second, third, fourth, fifth, sixth};
-    	targetFile.unread(returnBuffer);
-    	currentColumn = start;
-    	return entityReferenceLength(first,second,third,fourth,fifth,sixth);
+        int start = currentColumn;
+        char first = readNextChar();
+        char second = readNextChar();
+        char third = readNextChar();
+        char fourth = readNextChar();
+        char fifth = readNextChar();
+        char sixth = readNextChar();
+        char[] returnBuffer = {first, second, third, fourth, fifth, sixth};
+        targetFile.unread(returnBuffer);
+        currentColumn = start;
+        return entityReferenceLength(first,second,third,fourth,fifth,sixth);
     }
     
     String readEntityReference(int referenceLength) throws IOException{
-    	String entityReference = "";
-    	if (referenceLength >= 4) {
+        String entityReference = "";
+        if (referenceLength >= 4) {
             char first  = readNextChar();
             char second = readNextChar();
             char third  = readNextChar();
             char fourth = readNextChar();
             entityReference = entityReference + first + second +
-            		          third + fourth;
-    	}
-    	if (referenceLength >= 5) {
+                              third + fourth;
+        }
+        if (referenceLength >= 5) {
             char fifth = readNextChar();
             entityReference = entityReference + fifth;
-    	}
-    	if (referenceLength == 6) {
-    		char sixth = readNextChar();
-    		entityReference = entityReference + sixth;
-    	}
+        }
+        if (referenceLength == 6) {
+            char sixth = readNextChar();
+            entityReference = entityReference + sixth;
+        }
         return entityReference;
     }
     
     //closingQuote character only matters if value being read is an attribute
     //must be ' or "
     String readValue(boolean isAttributeValue, char closingQuote)
-    		         throws IOException{
+                     throws IOException{
         String value = "";
         int referenceLength;
         while (true){
             char nextChar = checkNextChar();
             if (isAttributeValue && (nextChar == closingQuote)) {
-            	readNextChar();
-            	return value;
+                readNextChar();
+                return value;
             }
             else if (isValueChar(nextChar)) {
                 readNextChar();
@@ -243,22 +243,22 @@ public class Validator{
             //of a new tag, so < can't occur in attributes, but signifies 
             //end of other values
             else if (nextChar == '<') 
-            	return (isAttributeValue)? "": value;
-	        else if (nextChar == '&') {
-	        	referenceLength = checkEntityReference();
+                return (isAttributeValue)? "": value;
+            else if (nextChar == '&') {
+                referenceLength = checkEntityReference();
                 if (referenceLength == 0)
-                	return "";
-		        value += readEntityReference(referenceLength);
+                    return "";
+                value += readEntityReference(referenceLength);
                 continue;
-	        }
-	        else //value not properly terminated
-	        	exitWithErrorMessage("unterminated value");
+            }
+            else //value not properly terminated
+                exitWithErrorMessage("unterminated value");
         }
     }
     
     String readAttributeValue() throws IOException{
-    	String attributeValue = "";
-    	char quote = (currentChar == '"')? '"' : '\'';
+        String attributeValue = "";
+        char quote = (currentChar == '"')? '"' : '\'';
         attributeValue += quote;
         attributeValue += readValue(true, quote);
         attributeValue += quote;
@@ -270,7 +270,7 @@ public class Validator{
         attribute += readAttributeName();
         attribute += readNextChar();
         if (currentChar != '=')
-        	return "";
+            return "";
         readNextChar();
         if (currentChar != '"' && currentChar != '\'')
             return "";
@@ -279,40 +279,40 @@ public class Validator{
     }
     
     String readAttributesIntoTag(String tag) throws IOException{
-	    while (checkNextChar() != '>') {
+        while (checkNextChar() != '>') {
             tag += readAttribute();
             tag += readWhiteSpace();
         }
-	    return tag;
+        return tag;
     }
    
     //modify to make sure it doesnt go on forever
     void ensureTagDoesNotBeginWithWhiteSpace() throws IOException{
-	    if (readWhiteSpace() != "")
-	 	   exitWithErrorMessage("tag whitespace");
+        if (readWhiteSpace() != "")
+           exitWithErrorMessage("tag whitespace");
     }
    
     String readTag(String tagType) throws IOException{
-	    String tag = "";
+        String tag = "";
         if (checkNextChar() != '<') {
             return "";
         }
         tag += readNextChar();
         if (checkNextChar() == '/') {
-    	    if (tagType == "opening") {
-    	 	    restoreEntity(tag);
-    	 	    return "";
-    	    }
-    	    else
-    	        tag += readNextChar();
+            if (tagType == "opening") {
+                restoreEntity(tag);
+                return "";
+            }
+            else
+                tag += readNextChar();
         }
         //tags can't have whitespace before the name
         ensureTagDoesNotBeginWithWhiteSpace();
         tag += readTagName();
         tag += readWhiteSpace();
         if ( (tagType == "closing") && (checkNextChar() != '>') ) {
-    	    restoreEntity(tag);
-    	    return "";
+            restoreEntity(tag);
+            return "";
         }
         tag = readAttributesIntoTag(tag);
         tag += readNextChar();
@@ -323,17 +323,17 @@ public class Validator{
         String tagName = "";
         boolean whitespaceInTag = false;
         for (int index = 0; index < tag.length(); index++) {
-        	if (tag.charAt(index) != ' ')
-        		tagName += tag.charAt(index);
-        	else {
-        		whitespaceInTag = true;
-        		break;
-        	}
+            if (tag.charAt(index) != ' ')
+                tagName += tag.charAt(index);
+            else {
+                whitespaceInTag = true;
+                break;
+            }
         }
         //if tag had no whitespace, > will have been added
         //otherwise, it must be added
         if (whitespaceInTag)
-        	tagName += '>';
+            tagName += '>';
         return tagName;
     }
     
@@ -346,22 +346,22 @@ public class Validator{
     }
     
     boolean pageHasEntity(String entityType) throws IOException{
-    	//read page to check for entity
+        //read page to check for entity
         //reading page changes current column
         //so column is saved, then reset
         String entity = "";
         int startColumn = currentColumn;
-    	switch (entityType){
-    	case "opening tag":
-    	    entity = readOpeningTag();
-    	    break;
-    	case "closing tag":
-    		entity = readClosingTag();
-    		break;
-    	case "value":
-    		entity = readValue(false,'"');
-    		break;
-    	}
+        switch (entityType){
+        case "opening tag":
+            entity = readOpeningTag();
+            break;
+        case "closing tag":
+           entity = readClosingTag();
+            break;
+        case "value":
+            entity = readValue(false,'"');
+            break;
+        }
         currentColumn = startColumn;
         restoreEntity(entity);
         return (entity != "");
@@ -371,31 +371,31 @@ public class Validator{
     String checkEntity(String entityType) throws IOException{
         String entity = "";
         int startColumn = currentColumn;
-	    switch (entityType){
-	    case "opening tag":
-	        entity = readOpeningTag();
-	        break;
-	    case "closing tag":
-		    entity = readClosingTag();
-		    break;
-	    case "value":
-		    entity = readValue(false,'"');
-		    break;
-	    }
+        switch (entityType){
+        case "opening tag":
+            entity = readOpeningTag();
+            break;
+        case "closing tag":
+            entity = readClosingTag();
+            break;
+        case "value":
+            entity = readValue(false,'"');
+            break;
+        }
         currentColumn = startColumn;
         restoreEntity(entity);
         return entity;
     }
     
     void restoreEntity(String entity) throws IOException{
-    	char[] pushbackEntity;
+        char[] pushbackEntity;
         pushbackEntity = entity.toCharArray();
         targetFile.unread(pushbackEntity);
     }
     
     boolean pageHasOpeningTag() throws IOException{
         return pageHasEntity("opening tag");
-    } 
+    }
     
     boolean pageHasClosingTag() throws IOException{
         return pageHasEntity("closing tag");
@@ -408,7 +408,7 @@ public class Validator{
     void popTag(String tagName){
         if (nestedTags.search(tagName) > 1)
             exitWithErrorMessage("invalid pop");
-	    else if (nestedTags.search(tagName) == -1)
+        else if (nestedTags.search(tagName) == -1)
             exitWithErrorMessage("no tag");
         else //search(tagName) == 1
             nestedTags.pop(); 
@@ -437,51 +437,51 @@ public class Validator{
             case "invalid name":
                 System.out.println("Invalid character for name");
                 break;
-	        case "invalid pop":
+            case "invalid pop":
                 System.out.println("Tag is improperly nested");
                 break;
-	        case "tag start":
+            case "tag start":
                 System.out.println("Tag must begin with opening bracket");
                 break;
-	        case "closing tag start":
-	        	System.out.println("First character in closing tag must" +
-	        			           " be forward slash");
+            case "closing tag start":
+                System.out.println("First character in closing tag must" +
+                                   " be forward slash");
                 break;
-	        case "closing tag end":
-	        	System.out.println("Closing tag can't have attributes, must" +
-	                               " end after tag name");
-	        	break;
-	        case "tag whitespace":
-	        	System.out.println("Tag must not have whitespace before tag name");
-	        	break;
-	        case "tag end":
+            case "closing tag end":
+                System.out.println("Closing tag can't have attributes, must" +
+                                   " end after tag name");
+                break;
+            case "tag whitespace":
+                System.out.println("Tag must not have whitespace before tag name");
+                break;
+            case "tag end":
                 System.out.println("Tag must end with closing bracket");
                 break;
-	        case "no tag":
+            case "no tag":
                 System.out.println("Tag was not found nested in stack");
                 break;
             case "unfound reference":
                 System.out.println("Entity reference not found");
                 break;
             case "attribute format":
-            	System.out.println("attribute consists of name, then '=', " + 
-            			           "then value");
-            	break;
+                System.out.println("attribute consists of name, then '=', " + 
+                                   "then value");
+                break;
             case "attribute start":
                 System.out.println("Attribute must start with quotation mark");
                 break;
-	        case "value character":
+            case "value character":
                 System.out.println("Illegal character found in value");
-	        case "unterminated value":
-	        	System.out.println("Value was not properly terminated");
+            case "unterminated value":
+                System.out.println("Value was not properly terminated");
                 break;
-	        case "unclosed root":
+            case "unclosed root":
                 System.out.println("Root element was not closed");
                 break;
-	        case "premature closure":
+            case "premature closure":
                 System.out.println("Root element was closed before end");
                 break;
-	    }
+        }
         System.exit(1);
     }
 }
